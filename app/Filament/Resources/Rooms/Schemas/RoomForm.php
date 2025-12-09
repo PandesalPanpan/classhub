38fd56f7bb3e4feb670e99f4ba55227a173ed2fc
PandesalPanpan <petermarticio@gmail.com>
2025-12-09 15:48:09 +0800

    Feat: Set a key slot number in a room

--- app/Filament/Resources/Rooms/Schemas/RoomForm.php
+++ app/Filament/Resources/Rooms/Schemas/RoomForm.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Resources\Rooms\Schemas;
 
+use App\KeyStatus;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Schema;
 
 class RoomForm
@@ -17,6 +20,17 @@ class RoomForm
                 Toggle::make('is_active')
                     ->required(),
                 TextInput::make('description'),
+                Fieldset::make('Key')
+                    ->relationship('key')
+                    ->schema([
+                        TextInput::make('slot_number')
+                            ->required(),
+                        Select::make('status')
+                            ->required()
+                            ->options(KeyStatus::class)
+                            ->default(KeyStatus::Disabled),
+                    ])
+                    ->columns(2),
             ]);
     }
 }
