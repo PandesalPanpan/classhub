36981b781ccf61c5cea95df6b52d7be8a161c3df
PandesalPanpan <petermarticio@gmail.com>
2025-12-12 16:52:56 +0800

    Feat: Register CalendarWidget in AppPanel

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -7,18 +7,16 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Saade\FilamentFullCalendar\FilamentFullCalendarPlugin;
 
 class AppPanelProvider extends PanelProvider
 {
@@ -54,6 +52,23 @@ class AppPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->plugin(
+                FilamentFullCalendarPlugin::make()
+                    ->schedulerLicenseKey('GPL-My-Project-Is-Open-Source')
+                    ->selectable(true)
+                    ->timezone(config('app.timezone'))
+                    ->plugins([
+                        'dayGrid',
+                        'timeGrid',
+                        'interaction',
+                        'list',
+                        'resource',
+                        'resourceTimeline',
+                    ])
+                    ->config([
+                        'initialView' => 'timeGridWeek',
+                    ])
+                );
     }
 }
--- resources/views/filament/pages/request-schedule.blade.php
+++ resources/views/filament/pages/request-schedule.blade.php
@@ -1,4 +1,5 @@
 <x-filament-panels::page>
     {{-- Page content --}}
+    @livewire(\App\Livewire\CalendarWidget::class)
     {{ $this->table }}
 </x-filament-panels::page>
