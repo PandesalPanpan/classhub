fa84601989034334b4f8f2a11f739ab91b57bd8e
PandesalPanpan <petermarticio@gmail.com>
2025-12-16 18:36:57 +0800

    Chore: Explicitly define policy to their models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Room;
+use App\Models\Schedule;
+use App\Policies\RoomPolicy;
+use App\Policies\SchedulePolicy;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Model::unguard();
+        Gate::policy(Schedule::class, SchedulePolicy::class);
+        Gate::policy(Room::class, RoomPolicy::class);
+
         // Model::preventLazyLoading();
     }
 }
