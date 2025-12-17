467a1421cb27df227976af1c1d48120576c7756f
PandesalPanpan <petermarticio@gmail.com>
2025-12-17 13:18:29 +0800

    Feat: Call RolesAndPermission Seeder in db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,5 +25,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoomSeeder::class,
         ]);
+
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+        ]);
     }
 }
